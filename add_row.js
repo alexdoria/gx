0c9f9a766ac0c2ab212f60a26f28c00b0570f5fd
Javier <sr.alexdoria@gmail.com>
2019-01-04 14:36:57 -0600

    Delete add_row.js
    This was just a test script for add_row.php

--- add_row.js
+++
@@ -1,3 +0,0 @@
-$("button#add_user_button").click(function(){
-	$("form#registry div.formgroup:last").after("<div class='formgroup'><label for='usremail'>Organizador</label><input type='text' class='form-control' id='usremail' name='usremail'></div>'")
-});
